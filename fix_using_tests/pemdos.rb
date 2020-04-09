ae20adde6a4114a945ea765605b96e9e699a57c3
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-09 21:40:18 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,8 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    # ruby is working differently with math operations depending on what the first data type is.
+  "s" * 10 + string
   else
     string
   end
